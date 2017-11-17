9c78fda19e1b47742423ebaef830b08c1baed6bf
Kun <pengzk@qwe.local>
2017-11-17 10:56:03 +0800

    NavigationBar

---
+++ NavigationBar/SwizzlingDefine.h
@@ -0,0 +1,25 @@
+//
+//  SwizzlingDefine.h
+//  utils
+//
+//  Created by PengZK on 2017/11/17.
+//  Copyright © 2017年 ZTC. All rights reserved.
+//
+
+#ifndef SwizzlingDefine_h
+#define SwizzlingDefine_h
+
+#import <objc/runtime.h>
+static inline void swizzling_exchangeMethod(Class clazz ,SEL originalSelector, SEL swizzledSelector){
+    Method originalMethod = class_getInstanceMethod(clazz, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(clazz, swizzledSelector);
+    
+    BOOL success = class_addMethod(clazz, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+    if (success) {
+        class_replaceMethod(clazz, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
+#endif /* SwizzlingDefine_h */
---
+++ NavigationBar/UILayoutFittingView.h
@@ -0,0 +1,13 @@
+//
+//  UILayoutFittingView.h
+//  职通车
+//
+//  Created by PengZK on 2017/10/10.
+//  Copyright © 2017年 ZTC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILayoutFittingView : UIView
+
+@end
