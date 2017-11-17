4439d6b699238cab2b9806c6c58b0040cd8e8dcd
Kun <pengzk@qwe.local>
2017-11-17 18:54:25 +0800

    add macros

---
+++ Macros/UtilMacros.h
@@ -0,0 +1,148 @@
+//
+//  UtilMacros.h
+//  Tools
+//
+//  Created by PengZK on 2017/11/17.
+//  Copyright © 2017年 ZTC. All rights reserved.
+//
+
+#ifndef UtilMacros_h
+#define UtilMacros_h
+
+#define MustOverride() @throw [NSException exceptionWithName:NSInvalidArgumentException reason:[NSString stringWithFormat:@"%s must be overridden in a subclass/category", __PRETTY_FUNCTION__] userInfo:nil]
+
+#if __has_feature(objc_arc) // ARC
+
+#define DEF_SINGLETON(name) \
+static id _instance; \
++ (id)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instance = [super allocWithZone:zone]; \
+}); \
+return _instance; \
+} \
+\
++ (instancetype)sharedInstance \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instance = [[self alloc] init]; \
+});\
+return _instance; \
+} \
++ (id)copyWithZone:(struct _NSZone *)zone \
+{ \
+return _instance; \
+}
+
+#else // 非ARC
+
+#define DEF_SINGLETON(name) \
+static id _instance; \
++ (id)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instance = [super allocWithZone:zone]; \
+}); \
+return _instance; \
+} \
+\
++ (instancetype)sharedInstance \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instance = [[self alloc] init]; \
+}); \
+return _instance; \
+} \
+\
+- (oneway void)release \
+{ \
+\
+} \
+\
+- (id)autorelease \
+{ \
+return _instance; \
+} \
+\
+- (id)retain \
+{ \
+return _instance; \
+} \
+\
+- (NSUInteger)retainCount \
+{ \
+return 1; \
+} \
+\
++ (id)copyWithZone:(struct _NSZone *)zone \
+{ \
+return _instance; \
+}
+
+#endif
+
+
+/*
+ * 全局函数定义宏
+ */
+#ifdef __cplusplus
+#define ZTC_EXTERN extern "C" __attribute__((visibility("default")))
+#else
+#define ZTC_EXTERN extern __attribute__((visibility("default")))
+#endif
+
+///神奇的 weak<-->strong dance 宏，记得成对使用
+#ifndef weakify
+#if DEBUG
+#if __has_feature(objc_arc)
+#define weakify(object) autoreleasepool{} __weak __typeof__(object) weak##_##object = object;
+#else
+#define weakify(object) autoreleasepool{} __block __typeof__(object) block##_##object = object;
+#endif
+#else
+#if __has_feature(objc_arc)
+#define weakify(object) try{} @finally{} {} __weak __typeof__(object) weak##_##object = object;
+#else
+#define weakify(object) try{} @finally{} {} __block __typeof__(object) block##_##object = object;
+#endif
+#endif
+#endif
+
+#ifndef strongify
+#if DEBUG
+#if __has_feature(objc_arc)
+#define strongify(object) autoreleasepool{} __typeof__(object) object = weak##_##object;
+#else
+#define strongify(object) autoreleasepool{} __typeof__(object) object = block##_##object;
+#endif
+#else
+#if __has_feature(objc_arc)
+#define strongify(object) try{} @finally{} __typeof__(object) object = weak##_##object;
+#else
+#define strongify(object) try{} @finally{} __typeof__(object) object = block##_##object;
+#endif
+#endif
+#endif
+
+
+///小巧耐用的主线程分发宏
+#define dispatch_main_sync_safe(block)\
+if ([NSThread isMainThread]) {\
+block();\
+} else {\
+dispatch_sync(dispatch_get_main_queue(), block);\
+}
+
+#define dispatch_main_async_safe(block)\
+if ([NSThread isMainThread]) {\
+block();\
+} else {\
+dispatch_async(dispatch_get_main_queue(), block);\
+}
+
+#endif /* UtilMacros_h */
