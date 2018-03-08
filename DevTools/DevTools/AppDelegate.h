21e3e598eeb9236287ff783d9c094c452da16ca3
Kun <pengzk@qwe.local>
2018-03-08 15:55:56 +0800

    upload xcode project

---
+++ DevTools/DevTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DevTools
+//
+//  Created by PengZK on 2018/3/8.
+//  Copyright © 2018年 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
